36c103d11af1dc300d38d902078e7d7271c52c0f
dadus <dadushegaonkar@gmail.com>
2018-08-23 22:49:11 +0530

    Added LoginController definition[23August2018]

---
+++ source/SlamBook/src/com/slambook/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.slambook.controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(value = "/login")
+public class LoginController extends HttpServlet {
+
+}
