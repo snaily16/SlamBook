9ceff3462a8d5b152fd4cc9631e2c7c62533ec2b
dadus <dadushegaonkar@gmail.com>
2018-08-23 22:53:50 +0530

    Added User model[23August2018]

---
+++ source/SlamBook/src/com/slambook/model/User.java
@@ -0,0 +1,27 @@
+package com.slambook.model;
+
+public class User {
+	private String firstName;
+	private String middleName;
+	private String surName;
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getMiddleName() {
+		return middleName;
+	}
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+	public String getSurName() {
+		return surName;
+	}
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+	
+
+}
